0c444920be3ab4626eaa6ba8f6f6328e00175477
ivanagushadinata06 <ivanagushadinata06@gmail.com>
2026-01-27 15:57:53 +0700

    Update petugas-tanggal.js

--- js/petugas-tanggal.js
+++ js/petugas-tanggal.js
@@ -1,18 +1,20 @@
 const params = new URLSearchParams(window.location.search);
-const bulan = parseInt(params.get("bulan"));
+const bulanIndex = parseInt(params.get("bulan"));
 const container = document.getElementById("tanggalContainer");
 
 const tahun = new Date().getFullYear();
-const lastDay = new Date(tahun, bulan + 1, 0).getDate();
+const lastDay = new Date(tahun, bulanIndex + 1, 0).getDate();
 
 for (let tgl = 1; tgl <= lastDay; tgl++) {
-  const date = new Date(tahun, bulan, tgl);
+  const date = new Date(tahun, bulanIndex, tgl);
 
-  if (date.getDay() === 0) { // Minggu
+  if (date.getDay() === 0) { // HARI MINGGU
     const iso = date.toISOString().split("T")[0];
 
     const btn = document.createElement("button");
     btn.innerText = `Minggu, ${tgl}`;
+    btn.style.marginBottom = "8px";
+
     btn.onclick = () => {
       window.location.href = `petugas-absen.html?tanggal=${iso}`;
     };
