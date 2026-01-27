65599ad7ee938a9fe0f99b80fce6b11dc17e0003
ivanagushadinata06 <ivanagushadinata06@gmail.com>
2026-01-27 14:24:05 +0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -3,3 +3,27 @@ function logout() {
     window.location.href = "./index.html";
   });
 }
+
+// Tambah jemaat
+function tambahJemaat() {
+  const nama = document.getElementById("namaJemaat").value;
+  if (!nama) return alert("Nama tidak boleh kosong");
+
+  db.collection("members").add({
+    name: nama
+  }).then(() => {
+    document.getElementById("namaJemaat").value = "";
+  });
+}
+
+// Tampilkan jemaat
+db.collection("members").onSnapshot(snapshot => {
+  const list = document.getElementById("listJemaat");
+  list.innerHTML = "";
+
+  snapshot.forEach(doc => {
+    const li = document.createElement("li");
+    li.innerText = doc.data().name;
+    list.appendChild(li);
+  });
+});
