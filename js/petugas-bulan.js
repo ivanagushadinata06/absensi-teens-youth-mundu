3019f9b2cf1470afc6d9116e1711a0d518e5d373
ivanagushadinata06 <ivanagushadinata06@gmail.com>
2026-01-27 16:25:39 +0700

    Update petugas-bulan.js

--- js/petugas-bulan.js
+++ js/petugas-bulan.js
@@ -1,9 +1,17 @@
+// Proteksi login
+auth.onAuthStateChanged(user => {
+  if (!user) {
+    window.location.href = "index.html";
+  }
+});
+
 const bulan = [
   "Januari","Februari","Maret","April","Mei","Juni",
   "Juli","Agustus","September","Oktober","November","Desember"
 ];
 
 const container = document.getElementById("bulanContainer");
+if (!container) return;
 
 bulan.forEach((nama, index) => {
   const btn = document.createElement("button");
