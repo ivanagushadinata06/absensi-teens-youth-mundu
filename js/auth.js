e4caf6ac23f41519def8dee02bbc95a03312409b
ivanagushadinata06 <ivanagushadinata06@gmail.com>
2026-01-27 16:06:30 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -7,18 +7,29 @@ function login() {
       const uid = userCredential.user.uid;
 
       db.collection("users").doc(uid).get().then(doc => {
-        if (!doc.exists) return;
+        if (!doc.exists) {
+          document.getElementById("error").innerText =
+            "Role user tidak ditemukan";
+          return;
+        }
 
         const role = doc.data().role;
-       if (role === "admin") {
-  window.location.href = "./admin.html";
-} else {
-  window.location.href = "./petugas.html";
-}
+
+        // =========================
+        // REDIRECT SESUAI ROLE
+        // =========================
+        if (role === "admin") {
+          window.location.href = "./admin.html";
+        } else if (role === "petugas") {
+          // 🔥 INI KUNCI PEMISAHAN HALAMAN
+          window.location.href = "./petugas-bulan.html";
+        } else {
+          document.getElementById("error").innerText =
+            "Role tidak dikenali";
+        }
       });
     })
     .catch(err => {
       document.getElementById("error").innerText = err.message;
     });
-
 }
